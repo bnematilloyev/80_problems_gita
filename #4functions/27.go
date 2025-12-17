40d82af2a5bceb2a10deb8f1bf0e33318cf20ebb
botir-nematilloyev <nematilloyevbro@gmail.com>
2025-12-17 13:36:29 +0500

    LCM

--- #4functions/27.go
+++ #4functions/27.go
@@ -1,14 +1,12 @@
 package main
 
-import "fmt"
-
 func lcm(a int, b int) int {
 	return a * b / gcd(a, b)
 }
 
-func main() {
-	var a, b int
-	fmt.Scan(&a, &b)
-	a, b = max(a, b), min(a, b)
-	fmt.Println(lcm(a, b))
-}
+//func main() {
+//	var a, b int
+//	fmt.Scan(&a, &b)
+//	a, b = max(a, b), min(a, b)
+//	fmt.Println(lcm(a, b))
+//}
