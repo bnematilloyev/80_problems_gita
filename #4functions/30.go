22297c1544b8e6c5f8f745825975ee04d6d89747
botir-nematilloyev <nematilloyevbro@gmail.com>
2025-12-17 14:01:02 +0500

    LeapYearCount

--- #4functions/30.go
+++ #4functions/30.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func isLeapYear(Y int) bool {
 	if Y%4 == 0 {
 		if Y%100 == 0 {
@@ -12,13 +10,13 @@ func isLeapYear(Y int) bool {
 	return false
 }
 
-func main() {
-	var x, count int
-	for i := 0; i < 3; i++ {
-		fmt.Scan(&x)
-		if isLeapYear(x) == true {
-			count++
-		}
-	}
-	fmt.Println(count)
-}
+//func main() {
+//	var x, count int
+//	for i := 0; i < 3; i++ {
+//		fmt.Scan(&x)
+//		if isLeapYear(x) == true {
+//			count++
+//		}
+//	}
+//	fmt.Println(count)
+//}
