732d62ef91f31736a6a497171da3909ca729cd4a
botir-nematilloyev <nematilloyevbro@gmail.com>
2025-12-17 14:21:36 +0500

    DaysInMonth

---
+++ #4functions/31.go
@@ -0,0 +1,44 @@
+package main
+
+func daysCountInMonth(month int) int {
+	switch month {
+	case 1:
+		return 31
+	case 2:
+		return 28
+	case 3:
+		return 31
+	case 4:
+		return 30
+	case 5:
+		return 31
+	case 6:
+		return 30
+	case 7:
+		return 31
+	case 8:
+		return 31
+	case 9:
+		return 30
+	case 10:
+		return 31
+	case 11:
+		return 30
+	case 12:
+		return 31
+	}
+	return 0
+}
+
+//func main() {
+//	var year, m int
+//	fmt.Scan(&year)
+//	for i := 0; i < 3; i++ {
+//		fmt.Scan(&m)
+//		if isLeapYear(year) == true && m == 2 {
+//			fmt.Println(29)
+//		} else {
+//			fmt.Println(daysCountInMonth(m))
+//		}
+//	}
+//}
