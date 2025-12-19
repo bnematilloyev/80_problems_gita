cccc6a9764cb8420d71814023333ca18747f8d69
botir-nematilloyev <nematilloyevbro@gmail.com>
2025-12-19 12:28:32 +0500

    Bubble sort

---
+++ #5array/40.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	fmt.Print(arr, "\n")
+}
